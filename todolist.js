c8cec4d90ef6c53fb47e477e4c2c154a08f0b7fc
Marko Knöbl <marko.kn@gmail.com>
2022-10-15 10:58:53 +0200

    general structure

--- todolist.js
+++ todolist.js
@@ -1,1 +1,45 @@
-console.log("Todolist");
+/*
+Todolist functionality:
+
+- welcome message
+- showing a list of completed / incomplete todos
+- adding a todo
+- deleting a todo
+- marking a todo as completed
+- statistics on completed / incomplete todos
+- deleting all completed todos
+*/
+
+let prompt = require("prompt-sync")();
+
+let todos = [
+  { name: "learn git", completed: false },
+  { name: "learn HTML", completed: true },
+  { name: "go shopping", completed: false },
+];
+
+function main() {
+  console.clear();
+  console.log("Welcome to the todo list app.");
+
+  let choice = "";
+  while (choice !== "q") {
+    //displayTodos();
+    console.log(
+      "What do you want to do?\n" +
+        "[a] add a todo\n" +
+        "[d] delete a todo\n" +
+        "[c] mark a todo as completed\n" +
+        "[q] quit"
+    );
+    choice = prompt();
+    if (choice === "a") {
+      addTodo();
+    } else if (choice === "d") {
+      deleteTodo();
+    }
+    // ...
+  }
+}
+
+main();
